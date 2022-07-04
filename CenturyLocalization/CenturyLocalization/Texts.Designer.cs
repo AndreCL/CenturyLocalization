be4ed30fffaa5e5d592c27f69ca852698430d7d7
André Castro <andre_lundin@outlook.com>
2022-07-04 13:12:32 +0200

    Included updates to warstab

--- CenturyLocalization/CenturyLocalization/Texts.Designer.cs
+++ CenturyLocalization/CenturyLocalization/Texts.Designer.cs
@@ -19,7 +19,7 @@ namespace CenturyLocalization {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Texts {
@@ -921,6 +921,15 @@ namespace CenturyLocalization {
         internal static string winner {
             get {
                 return ResourceManager.GetString("winner", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to World Tension.
+        /// </summary>
+        internal static string world_tension {
+            get {
+                return ResourceManager.GetString("world_tension", resourceCulture);
             }
         }
         
