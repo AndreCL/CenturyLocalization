e1ee68f7c5bd82e5fc88ca078c8d7ac49cd7c0c6
André Castro Lundin <andre.lundin@djoef.dk>
2026-01-06 13:55:58 +0100

    More translations

--- CenturyLocalization/CenturyLocalization/Actions/Actions.Designer.cs
+++ CenturyLocalization/CenturyLocalization/Actions/Actions.Designer.cs
@@ -19,7 +19,7 @@ namespace CenturyLocalization.Actions {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Actions {
@@ -156,6 +156,15 @@ namespace CenturyLocalization.Actions {
         internal static string remove_soldiers {
             get {
                 return ResourceManager.GetString("remove_soldiers", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Switch country name to {0}.
+        /// </summary>
+        internal static string switch_name_country {
+            get {
+                return ResourceManager.GetString("switch_name_country", resourceCulture);
             }
         }
     }
